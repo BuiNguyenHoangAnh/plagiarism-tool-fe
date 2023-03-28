7d500bbdad8f7c29d056497a09e71c0e62d2dc11
BuiNguyenHoangAnh <hoanganhbui099@gmail.com>
2023-03-28 23:37:49 +0700

    update UI for homepage

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CheckPlagiarismService } from '../check-plagiarism.service';
+import { FileUploadService } from '../file-upload.service';
 
 @Component({
   selector: 'app-main-page',
@@ -10,13 +11,28 @@ export class MainPageComponent implements OnInit {
   data: String = ''
   result: String = ''
 
-  constructor(private checkPlagiarism: CheckPlagiarismService) { }
+  context: any;
+
+  constructor(private checkPlagiarism: CheckPlagiarismService, private fileUploadService: FileUploadService) { }
 
   ngOnInit(): void {
   }
 
+  // ref: https://stackblitz.com/edit/angular-import-file?file=src%2Fapp%2Fapp.component.ts
+  openFile(event: any): void {
+    const input = event.target;
+    const reader = new FileReader();
+    reader.onload = (() => {
+      if (reader.result) {
+        this.context = JSON.parse(reader.result.toString())
+        console.log(JSON.parse(reader.result.toString()));
+      }
+    });
+    reader.readAsText(input.files[0], 'utf-8');
+  }
+
   check() {
-    this.checkPlagiarism.checkPlagiarism(this.data).subscribe((res:any)=>{
+    this.checkPlagiarism.checkPlagiarism(this.data).subscribe((res: any) => {
       this.result = res.result
     })
   }
